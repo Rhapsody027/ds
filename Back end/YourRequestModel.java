422b7176d7bc579c0f9ad4f7696064a75c91c668
Rhapsody027 <101376557+Rhapsody027@users.noreply.github.com>
2023-12-22 20:05:58 +0800

    Add files via upload

---
+++ Back end/YourRequestModel.java
@@ -0,0 +1,13 @@
+
+// 定義請求模型
+public class YourRequestModel {
+    private String[] results;
+
+    public String[] getResults() {
+        return results;
+    }
+
+    public void setResults(String[] results) {
+        this.results = results;
+    }
+}
